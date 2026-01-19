6091ee3ed05c4d581c4d08030c6733d7deb71451
Alberth Ruiz <ch.alb.rz@hotmail.com>
2026-01-19 10:11:31 -0500

    Update

--- server-scripts/run-cabify-weekly.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/toshify-sync/env.sh
-cd /opt/toshify-sync
-/root/.deno/bin/deno run --allow-net --allow-env sync-cabify-weekly.ts >> /var/log/sync-cabify-weekly.log 2>&1
--- server-scripts/run-uss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/toshify-sync/env.sh
-cd /opt/toshify-sync
-/root/.deno/bin/deno run --allow-net --allow-env sync-uss-excesos.ts >> /var/log/sync-uss.log 2>&1
